c524fa006b2430de6b59d54bd89a7f8d85b1d92d
shaker <tanimshaker@gmail.com>
2024-03-01 08:32:45 -0600

    added more answers and script.

--- script2.sh
+++ script2.sh
@@ -13,3 +13,8 @@ cat line_2.txt >> sonnet_1_copy.txt
 
 diff sonnet_1.txt sonnet_1_copy.txt
 
+
+#### Use cat to combine the contents of line_1.txt and line_2.txt in reverse order using a single command, yielding the file sonnet_1_reversed.txt. Hint: The cat command can take multiple arguments.
+
+cat line_2.txt line_1.txt > sonnet_1_reversed.txt
+
